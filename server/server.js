8d978667ec8c1b8cdd73c1d592be3197fd8ec36a
DhruvGaneriwal <108318029+DhruvGaneriwal@users.noreply.github.com>
2022-12-23 11:22:59 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// SETUP
+dotenv.config({ path: "./server/.env" });
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// DATABASE
+const db = process.env.MONGODB_CNN;
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB database connection established successfully!");
+  } catch (error) {
+    throw new Error("Database initialization error");
+  }
+};
+
+dbConnection();
+
+// CORS
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser("secretcode"));
+
+// ROUTES
+const authRouter = require("./routes/authRoutes");
+const dataRouter = require("./routes/dataRoutes");
+const newsRouter = require("./routes/newsRoutes");
+const stockRouter = require("./routes/stockRoutes");
+
+app.use("/api/auth", authRouter);
+app.use("/api/data", dataRouter);
+app.use("/api/news", newsRouter);
+app.use("/api/stock", stockRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+  });
+}
+
+// APP
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
